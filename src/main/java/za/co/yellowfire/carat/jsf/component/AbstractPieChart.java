e52cf718483cb156a36d0d55801981978a4a2463
markash <mp.ashworth@gmail.com>
2013-08-06 23:45:19 +0200

    Slow progress with the pie chart and Bootstrap 3-rc1 ;-(

--- src/main/java/za/co/yellowfire/carat/jsf/component/AbstractPieChart.java
+++ src/main/java/za/co/yellowfire/carat/jsf/component/AbstractPieChart.java
@@ -1,9 +1,6 @@
 package za.co.yellowfire.carat.jsf.component;
 
-import org.richfaces.cdk.annotations.JsfComponent;
-import org.richfaces.cdk.annotations.JsfRenderer;
-import org.richfaces.cdk.annotations.Tag;
-import org.richfaces.cdk.annotations.TagType;
+import org.richfaces.cdk.annotations.*;
 
 import javax.faces.component.UIComponentBase;
 
@@ -14,4 +11,7 @@ import javax.faces.component.UIComponentBase;
 public abstract class AbstractPieChart extends UIComponentBase {
     public static final String COMPONENT_FAMILY = "za.co.yellowfire.carat.chart";
     public static final String COMPONENT_TYPE = "za.co.yellowfire.carat.PieChart";
+
+    @Attribute
+    public abstract String getWidgetVar();
 }
--- src/main/java/za/co/yellowfire/carat/jsf/component/PieChartRendererBase.java
+++ src/main/java/za/co/yellowfire/carat/jsf/component/PieChartRendererBase.java
@@ -1,22 +1,33 @@
 package za.co.yellowfire.carat.jsf.component;
 
+import org.ajax4jsf.javascript.JSObject;
 import org.richfaces.renderkit.RendererBase;
 
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import java.io.IOException;
+import java.util.Locale;
 
 @ResourceDependencies({
     @ResourceDependency(library = "javax.faces", name = "jsf.js"),
     @ResourceDependency(name = "jquery.js", target = "head"),
-    @ResourceDependency(name = "richfaces.js", target = "head"),
-    @ResourceDependency(name = "richfaces-event.js", target = "head"),
-    @ResourceDependency(name = "richfaces-base-component.js", target = "head"),
-    @ResourceDependency(name = "ui.core.js", target = "head"),
-    @ResourceDependency(name = "ui.draggable.js", target = "head"),
-    @ResourceDependency(name = "ui.resizable.js", target = "head"),
-    @ResourceDependency(name = "fullcalendar.js", target = "head"),
-    @ResourceDependency(name = "richfaces.schedule.js", target = "head"),
-    @ResourceDependency(name = "fullcalendar.css", target = "head")})
+    @ResourceDependency(name = "jquery.flot.js", target = "head")
+})
 public abstract class PieChartRendererBase extends RendererBase {
     public static final String RENDERER_TYPE = "za.co.yellowfire.carat.jsf.PieChartRenderer";
+
+    protected void writeInitFunction(FacesContext context, UIComponent component) throws IOException {
+        AbstractPieChart chart = (AbstractPieChart) component;
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = chart.getClientId(context);
+        Locale locale = context.getViewRoot().getLocale();
+        String widgetVar = chart.getWidgetVar();
+        if (widgetVar != null) {
+            writer.writeText("var " + widgetVar + " = ", null);
+        }
+        writer.writeText(new JSObject("$.plot", clientId, "data", "options").toScript(), null);
+    }
 }
