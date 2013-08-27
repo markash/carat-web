2fdb8019c10d4b85a56cd40310fa3cafce68110e
markash <mp.ashworth@gmail.com>
2013-08-27 12:09:14 +0200

    Added source and javadoc plugins

--- src/main/java/za/co/yellowfire/carat/jsf/component/FormGroup.java
+++ src/main/java/za/co/yellowfire/carat/jsf/component/FormGroup.java
@@ -1,7 +1,14 @@
 package za.co.yellowfire.carat.jsf.component;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+
+import javax.annotation.Nullable;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponent;
 import javax.faces.component.UINamingContainer;
+import javax.validation.constraints.NotNull;
 
 @FacesComponent("za.co.yellowfire.carat.jsf.component.FormGroup")
 public class FormGroup extends UINamingContainer {
@@ -49,5 +56,42 @@ public class FormGroup extends UINamingContainer {
      */
     public void setLabel(java.lang.String label) {
         getStateHelper().put(PropertyKeys.label, label);
+    }
+
+    public String getValidationStyle() {
+        for(UIComponent child : getChildren()) {
+            System.out.println("child id = " + child.getId() + ", clientId = " + child.getClientId());
+        }
+
+        System.out.println("Reading messages for clientId " + getFor());
+
+        boolean hasError = hasMessages(getFor(), FacesMessage.SEVERITY_FATAL, FacesMessage.SEVERITY_ERROR);
+        boolean hasWarning = hasMessages(getFor(), FacesMessage.SEVERITY_WARN);
+//        boolean hasInfo = hasMessages(getFor(), FacesMessage.SEVERITY_INFO);
+
+        if (hasError) {
+            return "has-error";
+        } else if (hasWarning) {
+            return "has-warning";
+        }
+        return "";
+    }
+
+    protected boolean hasMessages(final String clientId, @NotNull final FacesMessage.Severity...severities) {
+        return !Iterables.isEmpty(Iterables.filter(
+                getFacesContext().getMessageList(clientId),
+                new Predicate<FacesMessage>() {
+                    @Override
+                    public boolean apply(@Nullable FacesMessage msg) {
+                        if (msg != null && msg.getSeverity() != null) {
+                            for (FacesMessage.Severity severity : severities) {
+                                if (severity.equals(msg.getSeverity())) {
+                                    return true;
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }));
     }
 }
